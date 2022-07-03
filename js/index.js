80d3994ce0d274f61ef92ded9d91d2f4a0cf74cb
Fernando <fernandoprada2014@gmail.com>
2022-07-02 22:33:56 -0500

    primer commit

---
+++ js/index.js
@@ -0,0 +1,64 @@
+
+    const $video = document.querySelector('#video');
+    const $play= document.querySelector('#play');
+    const $pause = document.querySelector('#pause');
+    const $backward = document.querySelector('#backward');
+    const $forward = document.querySelector('#forward');
+
+    $play.addEventListener('click',handlePlay);
+    $pause.addEventListener('click',handlePause);
+
+    function handlePlay(){
+        $video.play()
+        $play.hidden = true
+        $pause.hidden = false
+        console.log('click al boton play');
+    }
+
+    function handlePause(){
+        $video.pause()
+        $pause.hidden = true
+        $play.hidden = false
+        console.log('click al boton pausa');
+    }
+
+    $backward.addEventListener('click',handleBackward)
+    
+    function handleBackward(){
+
+        $video.currentTime = $video.currentTime - 10
+        console.log('click al boton retroceder 10 segundos',$video.currentTime);
+    }
+
+    $forward.addEventListener('click',handleForward)
+    
+    function handleForward(){
+
+        $video.currentTime = $video.currentTime + 10
+        console.log('click al boton avanzar 10 segundos',$video.currentTime);
+    }
+
+const $progress = document.querySelector('#progress');
+$video.addEventListener('loadedmetadata',handleLoaded)
+$video.addEventListener('timeupdate',handleTimeUpdate)
+
+function handleLoaded(){
+
+    $progress.max = $video.duration
+    console.log('ha cargado el video',$video.duration);
+
+}
+
+function handleTimeUpdate(){
+    $progress.value = $video.currentTime
+    // console.log('holis',$video.currentTime);
+}
+
+$progress.addEventListener('input',handleInput)
+
+function handleInput(){
+
+    $video.currentTime = $progress.value 
+    console.log($progress.value);
+}
+
